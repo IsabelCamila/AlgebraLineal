c8ae27a2239ed56212aa37fe596da519b0679800
Isabel Donoso Leiva <40239452+IsabelCamila@users.noreply.github.com>
2020-11-16 12:15:02 -0300

    Add files via upload

--- vector_v.js
+++ vector_v.js
@@ -5,18 +5,6 @@ class vector_v {
   mostrar() {
     strokeWeight(2);
     stroke(255, 0, 0);
-    line(0, 0, 0, this.v.x, -this.v.y, this.v.z)
-    push();
-    translate(this.v.x, -this.v.y, this.v.z);
-
-    if (this.v.y != 0) {
-      rotateZ(-HALF_PI-this.v.x*atan(this.v.x / this.v.y)/(abs(this.v.x)));
-    }
-    if (this.v.z != 0) {
-      rotateX(HALF_PI-this.v.x*atan(this.v.y / this.v.z)/(abs(this.v.x)));
-      rotateY(HALF_PI+this.v.x*atan(this.v.x / this.v.z)/(abs(this.v.x)));
-    }
-    cone(2, 5);
-    pop();
+    line(0, 0, 0, this.v.x, -this.v.y, this.v.z);
   }
 }
